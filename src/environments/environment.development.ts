bfb9be06ca44b3cd59fa9429f84e34be16e5d8bb
RafaelLeonTorres <r.leontorres1988@gmail.com>
2025-04-26 10:29:34 -0600

    ajuste variables nube

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production : false,
     apiURL: 'https://localhost:7202/api'
-    //apiURL: 'https://apipeliculas2-hfddeye6ava3cmhp.mexicocentral-01.azurewebsites.net/api'
 };
