e52c7ad7b25b46abc99eccf3da3fbd41ead66294
RafaelLeonTorres <r.leontorres1988@gmail.com>
2025-04-26 17:34:51 -0600

    ajustes para despliegue en azure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MenuComponent } from "./compartidos/componentes/menu/menu.component";
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from "./compartidos/componentes/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MenuComponent, RouterOutlet, FooterComponent],
+  imports: [MenuComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/compartidos/componentes/crear-entidad/crear-entidad.component.ts
+++ src/app/compartidos/componentes/crear-entidad/crear-entidad.component.ts
@@ -1,14 +1,13 @@
-import { formatCurrency } from '@angular/common';
 import { AfterViewInit, Component, ComponentRef, inject, Input, ViewChild, ViewContainerRef } from '@angular/core';
 import { IServicioCRUD } from '../../interfaces/IServicioCRUD';
 import { SERVICIO_CRUD_TOKEN } from '../../proveedores/proveedores';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { extraerErrores } from '../../funciones/extraerErrores';
 import { MostrarErroresComponent } from "../mostrar-errores/mostrar-errores.component";
 
 @Component({
   selector: 'app-crear-entidad',
-  imports: [RouterLink, MostrarErroresComponent],
+  imports: [MostrarErroresComponent],
   templateUrl: './crear-entidad.component.html',
   styleUrl: './crear-entidad.component.css'
 })
